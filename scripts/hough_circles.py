903f73e8067a5d4cb9f5a3ce82f4889fa4bddeb4
Kristoffer Nesland <kristoffernesland@gmail.com>
2019-03-18 12:14:51 +0100

    tidied in the scripts involving opencv

--- scripts/hough_circles.py
+++ scripts/hough_circles.py
@@ -21,13 +21,33 @@ def callback(data):
     try:
         cvImage = bridge.compressed_imgmsg_to_cv2(data, "bgr8")
     except CvBridgeError as e:
-      print(e)   
+      print(e)
+    
+    (rows, cols, channels) = cvImage.shape
+    grayImage = cv2.cvtColor(cvImage, cv2.COLOR_BGR2GRAY)
+
+    circles = cv2.HoughCircles(grayImage, cv2.HOUGH_GRADIENT, 1, 50, param1=50, param2=30, minRadius=0, maxRadius=200)
+
+    try:
+        numTopCircles = len(circles[0][0])
+        if numTopCircles > 5: # Maximum visualizing five circles.
+            numTopCircles = 5
+
+        topCircles = np.uint16(np.around(circles[0][0:(numTopCircles - 1)]))
+        
+        try:
+            for c in topCircles:
+                cv2.circle(cvImage, (c[0], c[1]), c[2], (0, 255, 0), 2)
+        except:
+            print("Circle out of bounds.")
+    except:
+        print("No circles found.")    
 
     cv2.imshow("Image window", cvImage)
     cv2.waitKey(3)
     
 def listener():
-    rospy.init_node('image_viewer', anonymous=True)
+    rospy.init_node('hough_circles', anonymous=True)
 
     if EXPERIMENT:
         rospy.Subscriber("/raspicam_node/image/compressed", CompressedImage, callback)
