65a3016c8e9cd302f3dff41ed99ec95557fa1421
Kristoffer Nesland <kristoffernesland@gmail.com>
2019-03-20 14:28:21 +0100

    added an outlier remover for the blue threshold image

--- scripts/thermal_center.py
+++ scripts/thermal_center.py
@@ -40,19 +40,47 @@ def callback(data):
     xMoment = np.sum(xm)
     yMoment = np.sum(ym)
 
+    # Trying to remove outliers. Only works when there is a single blue spot one is looking for. Maybe it makes it too slow? Lots of calculations.
     if not (total == 0):
         xArm = xMoment/total
         yArm = yMoment/total
+
+        xArm = int(math.floor(xArm))
+        yArm = int(math.floor(yArm))
+
+        sx = np.sqrt((np.sum(np.square(np.multiply(xv - xArm, mask))))/((np.sum(mask) - 1)))
+        sy = np.sqrt((np.sum(np.square(np.multiply(yv - yArm, mask))))/((np.sum(mask) - 1)))
+
+        zx = np.divide((xv - xArm), sx)
+        xMask = np.abs(zx) < 2.0
+
+        zy = np.divide((yv - yArm), sy)
+        yMask = np.abs(zy) < 1.8
+
+        xyMask = np.multiply(xMask, yMask)
+        mask = np.multiply(xyMask, mask)
+
+        xm = np.multiply(xv, mask)
+        ym = np.multiply(yv, mask)
+
+        total = np.sum(mask)
+        xMoment = np.sum(xm)
+        yMoment = np.sum(ym)
+
+
+    if not (total == 0):
+        xArm = xMoment/total
+        yArm = yMoment/total
+
+        xArm = int(math.floor(xArm))
+        yArm = int(math.floor(yArm))
+
+        cv.drawMarker(cvImage, (xArm, yArm), (0, 0, 255))
     else:
-        xArm = cols/2
-        yArm = rows/2
-
-    xArm = int(math.floor(xArm))
-    yArm = int(math.floor(yArm))
-
-    cv.drawMarker(cvImage, (xArm, yArm), (0, 0, 255))
+        print("No colour in the correct range was found.")
 
     cv.imshow("Image window", cvImage)
+    cv.imshow("Mask window", mask)
     cv.waitKey(3)
     
 def listener():
