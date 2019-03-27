4c527f7d843edbd0ece222fc605d3d8989bdb7d0
Kristoffer Nesland <kristoffernesland@gmail.com>
2019-03-27 12:55:00 +0100

    added connection symbol to navbar

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Eva
-
+(Early phase)
 An autonomous robot for use on an unmanned O&G production platform. Built on ROS and provides a web browser interface.
 
 ## Packages used
--- js/connect.js
+++ js/connect.js
@@ -5,13 +5,16 @@ var ros = new ROSLIB.Ros();
 // If there is an error on the backend, an 'error' emit will be emitted.
 ros.on('error', function(error) {
     console.log(error);
+    document.getElementById("connection-symbol").style.color = "#80d8ff";
 });
 // Find out exactly when we made a connection.
 ros.on('connection', function() {
     console.log('Connection made!');
+    document.getElementById("connection-symbol").style.color = "white";
 });
 ros.on('close', function() {
     console.log('Connection closed.');
+    document.getElementById("connection-symbol").style.color = "#80d8ff";
 });
 
 // Create a connection to the rosbridge WebSocket server.
