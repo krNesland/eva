4bf2d2493aa6f9afa3864ea1c3d7b17b01e9f8b1
Kristoffer Nesland <kristoffernesland@gmail.com>
2019-05-06 16:03:41 +0200

    mode1 works

--- js/click-map.js
+++ js/click-map.js
@@ -1,47 +1,38 @@
 
-// First, we create a Topic object with details of the topic's name and message type.
-var navTopic = new ROSLIB.Topic({
+var navigationClient = new ROSLIB.ActionClient({
     ros : ros,
-    name : 'move_base/goal',
-    messageType : 'move_base_msgs/MoveBaseActionGoal'
+    serverName : '/move_base',
+    actionName : 'move_base_msgs/MoveBaseAction'
 });
-
-var seqId = 0;
 
 // At click of map.
 function publishGoal(latlng) {
 
-    // Then we create the payload to be published. The object we pas in to ros.
-    var navMsg = new ROSLIB.Message({
-        goal: {
-            target_pose: {
-                header: {
-                    frame_id: "map"
+    var positionVec3 = new ROSLIB.Vector3({x:latlng.lng, y:latlng.lat, z:0});
+    var orientation = new ROSLIB.Quaternion({x:0, y:0, z:0, w:1.0});
+
+    var pose = new ROSLIB.Pose({
+        position : positionVec3,
+        orientation : orientation
+    });
+
+    var goal = new ROSLIB.Goal({
+        actionClient : navigationClient,
+        goalMessage : {
+            target_pose : {
+                header : {
+                    frame_id : '/map'
                 },
-                pose: {
-                    position: {
-                        // Have to do some trickery with the axis.
-                        x: latlng.lng,
-                        y: latlng.lat,
-                        z: 0.0
-                    },
-                    orientation: {
-                        x: 0.0,
-                        y: 0.0,
-                        z: 0.0,
-                        w: 1.0
-                    }
-                }
+                pose : pose
             }
-        },
-        goal_id: {
-            id: 'clickMapGoal_' + seqId
         }
     });
 
-    // And finally, publish.
-    navTopic.publish(navMsg);
-    seqId = seqId + 1;
+    goal.on('result', function(result) {
+        console.log("Goal reached or cancelled.");
+    });
+
+    goal.send();
 
     console.log("Sent message to navigate to (" + latlng.lat + ", " + latlng.lng + ").");
 }
